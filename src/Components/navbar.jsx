906f1698dc7b549c986b5ebab82d1c9cf56cb58f
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-09-29 23:21:30 +0530

    Navbar design saved

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,23 +1,33 @@
 import React from "react";
-
+import "../App.css";
 const NavBar = () => {
   return (
-    <nav class="navbar bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand">Navbar</a>
-        <form class="d-flex" role="search">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
+    <div className="py-2 bg-dark " id="nav-main">
+      <div className="flex-fill ms-3 pe-3" id="nav-left">
+        <input
+          className="form-control me-2 rounded-0 bg-secondary border-dark text-white"
+          type="search"
+          placeholder="Search 9,000+ tutorials"
+          aria-label="Search"
+        />
       </div>
-    </nav>
+      <div className="flex-fill " id="nav-mid">
+        <a className="align-middle navbar-brand text-white " href="/">
+          Navbar
+        </a>
+      </div>
+      <div className="flex-fill" id="nav-right">
+        <button
+          class="btn btn-sm text-white border-white rounded-0 fs-5"
+          type="button "
+        >
+          Menu
+        </button>
+        <button class="btn btn-sm btn-outline-secondary mx-2" type="button">
+          Sign In
+        </button>
+      </div>
+    </div>
   );
 };
 
