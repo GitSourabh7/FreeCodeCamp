9dc3be3b1c3b58344cb72fe866301df64794c63b
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-09-29 18:09:57 +0530

    Project initialize.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import NavBar from "./Components/navbar";
+
+class App extends Component {
+  state = {};
+  render() {
+    return <NavBar />;
+  }
+}
+
+export default App;
