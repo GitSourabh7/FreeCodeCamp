2ac2dcf154f1270c7f58af0ed3e1df2fd3e85ce8
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-09-30 15:49:03 +0530

    Design of Navbar  completed

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 import "../App.css";
+import { ReactComponent as Logo } from "../logo.svg";
+import { ReactComponent as Magnifier } from "../Icons/Magnifier.svg";
+
 const NavBar = () => {
   return (
-    <div className="py-2 bg-dark " id="nav-main">
-      <div className="flex-fill ms-3 pe-3" id="nav-left">
-        <input
-          className="form-control me-2 rounded-0 bg-secondary border-dark text-white"
-          type="search"
-          placeholder="Search 9,000+ tutorials"
-          aria-label="Search"
-        />
+    <div className="py-2 bg-black " id="nav-main">
+      <div
+        className="flex-fill ms-3 d-flex flex-column align-middle "
+        id="nav-left"
+      >
+        <div className="d-flex input-container">
+          <Magnifier id="magnifier" />
+          <input
+            className="form-control rounded-0 text-white px-1 d-flex align-middle fs-5"
+            id="input"
+            type="search"
+            placeholder="Search 9,000+ tutorials"
+            aria-label="Search"
+          />
+        </div>
       </div>
-      <div className="flex-fill " id="nav-mid">
-        <a className="align-middle navbar-brand text-white " href="/">
-          Navbar
+      <div className="flex-fill" id="nav-mid">
+        <a className="navbar-brand text-white" href="/">
+          <Logo className="d-flex-inline" />
         </a>
       </div>
       <div className="flex-fill" id="nav-right">
         <button
-          class="btn btn-sm text-white border-white rounded-0 fs-5"
+          class="btn btn-sm text-white border-white px-3 rounded-0 fs-5"
           type="button "
         >
           Menu
         </button>
-        <button class="btn btn-sm btn-outline-secondary mx-2" type="button">
+        <button
+          class="sign-in btn btn-sm btn-outline-secondary text-dark mx-3 fs-5 rounded-0 w-25"
+          type="button"
+        >
           Sign In
         </button>
       </div>
