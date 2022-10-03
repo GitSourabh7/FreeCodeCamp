913b81b711d1bc9ec0c83b60f5d23c53d0d257ba
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-10-03 20:10:07 +0530

    search input size fixed

--- src/Components/Common/buttonSignIn.jsx
+++ src/Components/Common/buttonSignIn.jsx
@@ -5,11 +5,6 @@ const ButtonSignIn = () => {
     <button className="d-grid sign-in gap-2 col-8 mx-auto fs-3" type="button">
       Get started (it's free)
     </button>
-    // <div class="d-grid gap-2 col-8 mx-auto">
-    //   <button class="sign-in btn fs-3 " type="button">
-    //     Get started (it's free)
-    //   </button>
-    // </div>
   );
 };
 
