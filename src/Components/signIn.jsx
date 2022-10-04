9c98b82936e097c6dbd8817a6ab2be744addbf0b
gitsourabh7 <sourabhnerlekar22@gmail.com>
2022-10-04 16:24:34 +0530

    Input box-shadow removed

---
+++ src/Components/signIn.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
